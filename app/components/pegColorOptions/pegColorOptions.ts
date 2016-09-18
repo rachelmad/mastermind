80cf8f9db43a00afd0eb97acb68a99139f5bf92e
rachelmad <rachelvmadrigal@gmail.com>
2016-09-18 02:38:03 -0500

    Added peg color options

---
+++ app/components/pegColorOptions/pegColorOptions.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { PegComponent } from '../peg/peg';
+import { COLOR1, 
+         COLOR2,
+         COLOR3,
+         COLOR4,
+         COLOR5,
+         COLOR6 } from '../../shared/constants';
+
+@Component({
+  selector: 'pegColorOptions',
+  templateUrl: 'build/components/pegColorOptions/pegColorOptions.html',
+  directives: [
+    PegComponent
+  ]
+})
+export class PegColorOptionsComponent {
+  private color1: string = COLOR1;
+  private color2: string = COLOR2;
+  private color3: string = COLOR3;
+  private color4: string = COLOR4;
+  private color5: string = COLOR5;
+  private color6: string = COLOR6;
+  
+  constructor() {
+  }
+}
+
---
+++ app/shared/constants.ts
@@ -0,0 +1,6 @@
+export const COLOR1 = 'red';
+export const COLOR2 = 'orange';
+export const COLOR3 = 'yellow';
+export const COLOR4 = 'green';
+export const COLOR5 = 'blue';
+export const COLOR6 = 'purple';
