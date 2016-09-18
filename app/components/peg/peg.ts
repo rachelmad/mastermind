e48bddb453365b75e8168b8eb0548a2fc2814066
rachelmad <rachelvmadrigal@gmail.com>
2016-09-18 01:22:45 -0500

    Added color options to peg component

--- app/components/peg/peg.ts
+++ app/components/peg/peg.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: "peg",
   templateUrl: 'build/components/peg/peg.html'
 })
-export class PegComponent {
+export class PegComponent implements OnInit {
+  @Input() firstColor: string;
+
+  private pegColor;
+
+  ngOnInit() {
+    this.pegColor = this.firstColor;
+  }
 }
