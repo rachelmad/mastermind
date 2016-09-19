24152293f72b64b5b9114611917abfe079048cc3
rachelmad <rachelvmadrigal@gmail.com>
2016-09-19 00:25:26 -0500

    Fixed hint logic where repeat colors give wrong hints

--- app/components/turnSet/turnSet.ts
+++ app/components/turnSet/turnSet.ts
@@ -23,6 +23,7 @@ const NO_MATCH = 0;
 export class TurnSetComponent {
   @Input() code: Array<string>;
   
+  private codeCopy: Array<string>;
   private hints: Array<number> = []; 
   private checked: Array<boolean>;
   private hasHints: boolean = false;
@@ -30,26 +31,62 @@ export class TurnSetComponent {
   checkPattern(pattern: Array<string>): void {
     let position = 0;
     this.checked = [false, false, false, false];
-    pattern.forEach((peg: string) => {
-      this.checkPeg(peg, position);
-      position++;
-    })
+    this.codeCopy = this.code.slice();
+
+    this.getPositionMatches(pattern);
+    let newPattern = this.removeMatchedPegs(pattern);
+    let newCode = this.removeMatchedPegs(this.codeCopy);
+    this.getColorMatches(newPattern, newCode);
+
     this.hasHints = true;
   }
 
-  checkPeg(color: string, position: number): void {
-    let currentPosition = 0;
-    this.code.forEach((peg: string) => {
-      if (color === peg && this.checked[currentPosition] === false) {
-        if (position === currentPosition) {
-          this.hints.push(POSITION_MATCH);
-        }
-        else {
-          this.hints.push(COLOR_MATCH);
-        }
-        this.checked[currentPosition] = true;
-      }
-      currentPosition++;
+  getPositionMatches(pattern: Array<string>): void {
+    if (this.code[0] === pattern[0]) {
+      this.hints.push(POSITION_MATCH);
+      this.checked[0] = true;
+    }
+    if (this.code[1] === pattern[1]) {
+      this.hints.push(POSITION_MATCH);
+      this.checked[1] = true;
+    }
+    if (this.code[2] === pattern[2]) {
+      this.hints.push(POSITION_MATCH);
+      this.checked[2] = true;
+    }
+    if (this.code[3] === pattern[3]) {
+      this.hints.push(POSITION_MATCH);
+      this.checked[3] = true;
+    }
+  }
+
+  removeMatchedPegs(pattern: Array<string>): Array<string> {
+    let index = 0;
+    let newPattern: Array<string> = pattern;
+    this.checked.forEach((check: boolean) => {
+      if (check) {
+        newPattern = pattern.splice(index, 1);
+      };
+      index++;
     })
+    return newPattern;
+  }
+
+  getColorMatches(pattern: Array<string>, code: Array<string>) {
+    let matched: Array<boolean> = [];
+    pattern.forEach(() => {
+      matched.push(false);
+    })
+
+    let index = 0;
+    pattern.forEach((patternPeg: string) => {
+      code.forEach((codePeg: string) => {
+        if (codePeg === patternPeg && matched[index] === false) {
+          this.hints.push(COLOR_MATCH);
+          matched[index] = true;
+        };
+        index++;
+      });
+    });
   }
 }
