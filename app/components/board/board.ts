7bce2a903e4211ac504a6527837af849accbb76d
rachelmad <rachelvmadrigal@gmail.com>
2016-09-18 21:51:07 -0500

    Added secret code logic

--- app/components/board/board.ts
+++ app/components/board/board.ts
@@ -15,5 +15,29 @@ import { COLOR1,
   ]
 })
 export class BoardComponent {
-  private code: Array<string> = [COLOR1, COLOR2, COLOR3, COLOR4];
+  private code: Array<string> = [];
+
+  constructor() {
+    for (var i = 0; i < 4; i++) {
+      this.code.push(this.convertNumberToColor(Math.floor(Math.random() * 6) + 1));
+    };
+    console.log(this.code);
+  }
+
+  convertNumberToColor(num: number) {
+    switch (num) {
+      case 1:
+        return COLOR1;
+      case 2:
+        return COLOR2;
+      case 3:
+        return COLOR3;
+      case 4:
+        return COLOR4;
+      case 5:
+        return COLOR5;
+      case 6:
+        return COLOR6;
+    }
+  }
 }
